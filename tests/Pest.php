cf76df955bd15f68dfc7140271819c39e9256373
Marcos "Marcão" Aurelio <macosaures+noreply@gmail.com>
2026-08-12 23:42:35 -0300

    🎨 refactor(license): remove linha em branco extra do cabeçalho
    - src/registration.php: remove linha em branco extra do cabeçalho gerado por php-cs-fixer
    - tests/Pest.php: remove linha em branco extra do cabeçalho gerado por php-cs-fixer
    - tests/TestCase.php: remove linha em branco extra do cabeçalho gerado por php-cs-fixer
    - tests/Feature/ExampleTest.php: remove linha em branco extra do cabeçalho gerado por php-cs-fixer
    - tests/Unit/ExampleTest.php: remove linha em branco extra do cabeçalho gerado por php-cs-fixer

--- tests/Pest.php
+++ tests/Pest.php
@@ -6,7 +6,6 @@ declare(strict_types=1);
  * Copyright (c) 2026 MACO Studios in colaboration with Gubee
  *
  * @see https://github.com/maco-studios/gubee-module-gubee-integration
- *
  */
 
 use Tests\TestCase;
